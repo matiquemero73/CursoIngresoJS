42562656e23eba146dfbd421ebe9c7510f031233
alumno <alumno>
2017-02-07 12:29:03 -0300

    GUARDADO 3.0

--- 2-InstruccionIf/jsInstruccionIF (3).js
+++ 2-InstruccionIf/jsInstruccionIF (3).js
@@ -1,5 +1,18 @@
 function Mostrar()
-{
+{ 
+    var edad;
+    var edad=document.getElementById("edad").value;
+    if(edad>=18){
+    alert("es mayor de edad ")
+
+    }
+    
+    if(edad<18){ 
+
+    alert("es menor de edad ")
+    }
+
+
 //tomo la edad  
 
 
